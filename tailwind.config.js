54797679899b07d5d1f0cf10d26edf0beb8771d6
Joshua Ellman <joshalanellman@gmail.com>
2025-07-14 19:51:01 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  ],  
   theme: {
     extend: {
       // Custom color palette matching your original style.css variables
